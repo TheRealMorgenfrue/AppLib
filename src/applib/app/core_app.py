f4104f3a8ee706a3536677c21f9976a8885ad649
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2025-01-02 22:15:28 +0100

    Move path setting to main app file

--- src/applib/app/core_app.py
+++ src/applib/app/core_app.py
@@ -17,31 +17,6 @@ class CoreApp:
         MainWindow : QWidget
             A reference to the main window class of the GUI.
         """
-        self.MainWindow = MainWindow
-        self.setupPath()
-        self.run()
-
-    def setupPath(self) -> None:
-
-        ##########################
-        ### Initial Path Setup ###
-        ##########################
-        # Set initial CWD
-        os.chdir(Path(os.path.abspath(__file__)).parents[3])
-
-        # Running in a Nuitka onefile binary
-        if Path(sys.argv[0]) != Path(__file__):
-            setattr(sys, "nuitka", True)
-
-        # # Running in a PyInstaller bundle
-        # elif getattr(sys, "frozen", False):
-        #     pass
-        # # Running in a normal python process
-        # else:
-        #     pass
-        ##########################
-
-    def run(self) -> None:
         # enable dpi scale
         QApplication.setHighDpiScaleFactorRoundingPolicy(
             Qt.HighDpiScaleFactorRoundingPolicy.PassThrough
@@ -51,7 +26,7 @@ class CoreApp:
         app.setAttribute(Qt.ApplicationAttribute.AA_DontCreateNativeWidgetSiblings)
 
         try:
-            w = self.MainWindow()
+            w = MainWindow()
             sys.exit(app.exec())
         except Exception:
             import traceback
