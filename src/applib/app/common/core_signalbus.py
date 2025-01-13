832d4429e8db5e0414867d4c5bce20c88eb8de62
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2025-01-14 00:12:15 +0100

    Add documentation. Add parent key to config update signals

--- src/applib/app/common/core_signalbus.py
+++ src/applib/app/common/core_signalbus.py
@@ -5,51 +5,133 @@ class CoreSignalBus(QObject):
     # ───────────────────────────────────────────────────────────────────────────#
     # Config updates
     # ───────────────────────────────────────────────────────────────────────────#
-
-    # isSuccess: bool, title: str, content: str
-    # Notify whether a value was inserted in a config successfully or not
     configStateChange = pyqtSignal(bool, str, str)
+    """
+    Notify whether a value was inserted in a config successfully or not.
 
-    # config_name: str, config_key: str, value: tuple[Any]
-    # Notify that a value in a config is updated
-    # The tuple is a 1-tuple with the setting's value, e.g. (value,)
-    configUpdated = pyqtSignal(str, str, tuple)
+    Parameters
+    ----------
+    is_success : bool
+        The value was succesfully inserted in the config.
 
-    # old_config_name: str, new_config_name: str
+    title : str
+        Title of GUI message.
+
+    content : str
+        Content of GUI message.
+    """
+
+    configUpdated = pyqtSignal(str, str, tuple, tuple)
+    """
+    Notify that a value in a config is updated.
+
+    Parameters
+    ----------
+    config_name : str
+        The name of the config.
+
+    config_key : str
+        The key whose value was updated.
+
+    parent_key : tuple[str | None]
+        The parent key of `config_key`.
+
+    value : tuple[Any]
+        The updated value mapped to `config_key`.
+    """
+
     configNameUpdated = pyqtSignal(str, str)
+    """
+    Notify that the name of a config is updated.
 
-    # config_name: str
-    # Force the config to be saved to disk
+    Parameters
+    ----------
+    old_config_name : str
+        The old config name.
+
+    new_config_name : str
+        The new config name.
+    """
+
     doSaveConfig = pyqtSignal(str)
+    """
+    Force the config to be saved to disk.
+
+    Parameters
+    ----------
+    config_name : str
+        The name of the config.
+    """
 
     # ───────────────────────────────────────────────────────────────────────────#
     # Setting updates
     # ───────────────────────────────────────────────────────────────────────────#
+    updateConfigSettings = pyqtSignal(str, str, tuple, tuple)
+    """
+    Update a key's value programmatically both in config and GUI.
 
-    # config_name: str, config_key: str, value: tuple[Any]
-    # Update a setting's value programmatically from anywhere
-    # The tuple is a 1-tuple with the setting's value, e.g. (value,)
-    updateConfigSettings = pyqtSignal(str, str, tuple)
+    NOTE: Before emitting, it is required to have instantiated at least one GUI representation of a config using any Generator.
+
+    Parameters
+    ----------
+    config_name : str
+        The name of the config.
+
+    config_key : str
+        The key whose value should be updated.
+
+    parent_key : tuple[str | None]
+        The parent key of `config_key`.
+
+    value : tuple[Any]
+        The updated value mapped to `config_key`.
+    """
 
     # ───────────────────────────────────────────────────────────────────────────#
     # Errors
     # ───────────────────────────────────────────────────────────────────────────#
-
-    # title: str, content: str
-    # Notify the user that a generic program error has occurred
     genericError = pyqtSignal(str, str)
+    """
+    Notify the user that a generic program error has occurred.
 
-    # config_name: str, title: str, content: str
-    # Notify the user that a config validation error occured
+    Parameters
+    ----------
+    title : str
+        Title of GUI message.
+
+    content : str
+        Content of GUI message.
+    """
+
     configValidationError = pyqtSignal(str, str, str)
+    """
+    Notify the user that a config validation error occured.
+
+    Parameters
+    ----------
+    config_name : str
+        The name of the config.
+
+    title : str
+        Title of GUI message.
+
+    content : str
+        Content of GUI message.
+    """
 
     # ───────────────────────────────────────────────────────────────────────────#
     # General
     # ───────────────────────────────────────────────────────────────────────────#
-
-    # isRunning: bool
-    # Notify whether processes are running in a ThreadManager
     isProcessesRunning = pyqtSignal(bool)
+    """
+    Notify whether processes are running in a ThreadManager.
+
+    NOTE: Deprecated and scheduled for removal.
+
+    Parameters
+    ----------
+    isRunning : bool
+    """
 
 
 core_signalbus = CoreSignalBus()
