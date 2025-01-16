ab68b992739c7a9a06d03236c3f9b4e1da608b1f
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2025-01-16 01:44:05 +0100

    Add test text

--- src/test/modules/config/templates/test_template.py
+++ src/test/modules/config/templates/test_template.py
@@ -123,6 +123,7 @@ class TestTemplate(BaseTemplate):
                 },
                 "createDownloadLists": {
                     "ui_title": "Automatically create download-lists",
+                    "ui_desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                     "default": False,
                     "ui_group_parent": [
                         UIGroups.NESTED_CHILDREN,
@@ -134,7 +135,8 @@ class TestTemplate(BaseTemplate):
             },
             "Settings": {
                 "downloadListDirectory": {
-                    "ui_title": "Folder for download lists.",
+                    "ui_title": "Folder for download lists",
+                    "ui_desc": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt",
                     "ui_invalidmsg": {"title": "", "desc": ""},
                     "default": "",
                     "validators": [validatePath],
