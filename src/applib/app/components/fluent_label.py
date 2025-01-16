8b659034cc82524dde038b05695c76e56e0d31c6
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2025-01-16 03:29:11 +0100

    Improve width calculation

--- src/applib/app/components/fluent_label.py
+++ src/applib/app/components/fluent_label.py
@@ -1,13 +1,10 @@
-from typing import Optional
-from PyQt6.QtCore import Qt, QSize, QRect
-from PyQt6.QtWidgets import QLabel, QWidget, QStyle, QSizePolicy
+from PyQt6.QtCore import Qt, QSize
+from PyQt6.QtWidgets import QLabel, QWidget, QSizePolicy
 
-from ...module.logging import AppLibLogger
+from typing import Optional
 
 
 class FluentLabel(QLabel):
-    test_logger = AppLibLogger().getLogger()
-
     def __init__(
         self,
         text: Optional[str],
@@ -18,37 +15,6 @@ class FluentLabel(QLabel):
             QSizePolicy.Policy.Preferred, QSizePolicy.Policy.MinimumExpanding
         )
 
-    def _documentRect(self) -> QRect:
-        """
-        Compute document rect which sometimes is more accurate than
-        boundingRect() (which has a tendency to underestimate the rect)
-
-        Code adapted from 'QLabelPrivate::documentRect()' at:
-          https://code.qt.io/cgit/qt/qtbase.git/tree/src/widgets/widgets/qlabel.cpp
-
-        Returns
-        -------
-        QRect
-            The document rect where the label's text is drawn
-        """
-        cr = self.contentsRect()
-        margin = self.margin()
-        cr.adjust(margin, margin, -margin, -margin)
-        align = QStyle.visualAlignment(self.layoutDirection(), self.alignment())
-        indent = self.indent()
-        if indent < 0 and self.frameWidth():  # No indent, but we do have a frame
-            indent = self.fontMetrics().horizontalAdvance("x") / 2 - margin
-        if indent > 0:
-            if align | Qt.AlignmentFlag.AlignLeft:
-                cr.setLeft(cr.left() + indent)
-            if align | Qt.AlignmentFlag.AlignRight:
-                cr.setRight(cr.right() - indent)
-            if align | Qt.AlignmentFlag.AlignTop:
-                cr.setTop(cr.top() + indent)
-            if align | Qt.AlignmentFlag.AlignBottom:
-                cr.setBottom(cr.bottom() - indent)
-        return cr
-
     def sizeHint(self) -> QSize:
         self.updateGeometry()
         if self.isHidden() or not self.text():
@@ -56,13 +22,7 @@ class FluentLabel(QLabel):
 
         # Compute width
         p = self.parentWidget()
-        if p:
-            w = p.width()
-        else:
-            shW = super().sizeHint().width()
-            rctW = self.fontMetrics().boundingRect(self.text()).width()
-            docW = self._documentRect().width()
-            w = max(rctW, docW, shW)
+        w = p.width() if p else self.fontMetrics().horizontalAdvance(self.text())
 
         # Compute height
         www = (
