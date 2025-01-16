6785d93ca48fd19355ad6483f595a85cdfecb2bc
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2025-01-16 01:41:42 +0100

    Cleanup

--- src/applib/app/components/settingcards/cards/__init__.py
+++
@@ -1,9 +0,0 @@
-from .clustered_settingcard import ClusteredSettingCard
-from .expanding_settingcard import ExpandingSettingCard
-from .scroll_settingcardgroup import ScrollSettingCardGroup
-from .settingcard import (
-    FlowSettingCard,
-    FluentSettingCard,
-    FormSettingCard,
-    GenericSettingCard,
-)
--- src/applib/app/components/settingcards/widgets/__init__.py
+++
@@ -1,7 +0,0 @@
-from .cardwidgetgroup import CardWidgetGroup
-from .parent_settingwidgets import (
-    NestedSettingWidget,
-    ClusteredSettingWidget,
-    ParentSettingWidget,
-)
-from .settingwidget import SettingWidget, SettingWidgetBase
