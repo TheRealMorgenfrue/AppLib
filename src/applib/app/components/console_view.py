1fd801299360f4b87920c66390d14fd9075e485c
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2025-06-23 14:35:29 +0200

    Implemented dynamic scrolling and added color support to console

--- src/applib/app/components/console_view.py
+++ src/applib/app/components/console_view.py
@@ -1,41 +1,40 @@
-from qfluentwidgets import PrimaryPushButton, TextEdit
-from PyQt6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QLabel, QTextEdit
-from PyQt6.QtGui import QColor
 from PyQt6.QtCore import QSize, Qt, pyqtSignal
-
-from typing import Optional
+from PyQt6.QtGui import QColor, QFont
+from PyQt6.QtWidgets import QHBoxLayout, QLabel, QTextEdit, QVBoxLayout, QWidget
+from qfluentwidgets import PrimaryPushButton
 
 from ..common.core_stylesheet import CoreStyleSheet
+from .smart_text_edit import SmartTextEdit
 
 
 class ConsoleView(QWidget):
-    terminationRequest = pyqtSignal(int)
+    terminate_process = pyqtSignal(int)
     activated = pyqtSignal(bool)
 
     def __init__(
         self,
         process_id: int,
-        sizeHint: Optional[QSize] = None,
-        parent: Optional[QWidget] = None,
+        sizeHint: QSize | None = None,
+        parent: QWidget | None = None,
     ) -> None:
         super().__init__(parent)
-        self._sizeHint = sizeHint if sizeHint else QSize(400, 400)
+        self._sizeHint = sizeHint if sizeHint is not None else QSize(400, 400)
         self.process_id = process_id
         self.consoleLabel = QLabel(self.tr(f"Thread {process_id}"))
-        self.textEdit = TextEdit(self)
+        self.textEdit = SmartTextEdit(self)
         self.terminateButton = PrimaryPushButton(self.tr("Terminate"), self)
         self.vBoxLayout = QVBoxLayout(self)
         self.buttonLayout = QHBoxLayout()
 
         self._initWidget()
         self._initLayout()
-        self._connectSignalToSlot()
+        self.__connectSignalToSlot()
 
     def _initWidget(self) -> None:
         self.textEdit.setLineWrapMode(QTextEdit.LineWrapMode.NoWrap)
         self.textEdit.setReadOnly(True)
         # Block count == Line count. NOTE: Also disables undo/redo history.
-        self.textEdit.document().setMaximumBlockCount(1000)
+        self.textEdit.document().setMaximumBlockCount(1000)  # type: ignore
         self.terminateButton.setDisabled(True)
         self._setQss()
 
@@ -55,18 +54,27 @@ class ConsoleView(QWidget):
         self.setObjectName("console")
         CoreStyleSheet.CONSOLE_VIEW.apply(self)
 
-    def _connectSignalToSlot(self) -> None:
+    def __connectSignalToSlot(self) -> None:
         self.terminateButton.clicked.connect(
-            lambda: self.terminationRequest.emit(self.process_id)
+            lambda: self.terminate_process.emit(self.process_id)
         )
-        self.activated.connect(self.terminateButton.setEnabled)
+        self.activated.connect(self._onActivated)
+
+    def _onActivated(self, activated: bool):
+        if self.terminateButton.isEnabled() != activated:
+            self.terminateButton.setEnabled(activated)
 
     def insert(self, text: str) -> None:
         self.textEdit.insertPlainText(text)
 
-    def append(self, text: str, color: Optional[QColor] = None) -> None:
+    def append(
+        self, text: str, color: QColor | None = None, bold: bool = False
+    ) -> None:
         if color:
             self.textEdit.setTextColor(color)
+        if bold:
+            self.textEdit.setFontWeight(QFont.Weight.Bold)
+
         self.textEdit.append(text.strip())
 
     def clear(self) -> None:
