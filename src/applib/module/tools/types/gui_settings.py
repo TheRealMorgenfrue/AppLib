525e1cf535f52482fc5f64eb3b243b7a4a4a241c
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2025-06-23 14:53:19 +0200

    Add FileSelect to types

--- src/applib/module/tools/types/gui_settings.py
+++ src/applib/module/tools/types/gui_settings.py
@@ -3,6 +3,7 @@ from typing import TypeAlias
 from ....app.components.settings.checkbox import CoreCheckBox
 from ....app.components.settings.color_picker import CoreColorPicker
 from ....app.components.settings.combobox import CoreComboBox
+from ....app.components.settings.file_selection import CoreFileSelect
 from ....app.components.settings.line_edit import CoreLineEdit
 from ....app.components.settings.slider import CoreSlider
 from ....app.components.settings.spinbox import CoreSpinBox
@@ -11,6 +12,7 @@ from ....app.components.settings.switch import CoreSwitch
 AnySetting: TypeAlias = (
     CoreColorPicker
     | CoreComboBox
+    | CoreFileSelect
     | CoreLineEdit
     | CoreSlider
     | CoreSpinBox
