977fc4d33540824a8d959f1215367801d87b3a74
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2026-02-23 23:39:20 +0100

    Improve docstring

--- src/applib/module/configuration/runners/converters/converter.py
+++ src/applib/module/configuration/runners/converters/converter.py
@@ -8,20 +8,24 @@ class Converter:
     @abstractmethod
     def convert(self, value: Any, to_gui: bool = False) -> Any:
         """
-        Convert `value` between config and GUI representation.
+        Convert `value` between types.
+        For instance, converting a string "0" to the number 0.
 
         Parameters
         ----------
         value : Any
-            The value to convert to an appropriate type.
+            The value to convert.
 
         to_gui : bool, optional
-            Convert value to a type suitable for the GUI.
+            Whether to return the converted value.
             By default False.
 
         Returns
         -------
         Any
-            `value` converted to a suitable type.
+            If `to_gui` is True:
+                return `value` converted to the GUI type
+            else:
+                return `value`
         """
         ...
