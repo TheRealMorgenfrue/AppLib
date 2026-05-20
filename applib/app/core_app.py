d51cafaf12d8247d8d0eb33242511ecef040bb45
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2026-05-20 14:47:45 +0200

    Restructure project to follow project layout conventions

---
+++ applib/app/core_app.py
@@ -0,0 +1,52 @@
+import os
+import sys
+from pathlib import Path
+
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QApplication, QWidget
+
+
+class CoreApp:
+    def __init__(self, MainWindow: type[QWidget]) -> None:
+        """
+        The main class from which all other code is executed.
+
+        NOTE: Only one instance of this class may be running.
+
+        Parameters
+        ----------
+        MainWindow : QWidget
+            A reference to the main window class of the GUI.
+        """
+        try:
+            # Enable dpi scale
+            QApplication.setHighDpiScaleFactorRoundingPolicy(
+                Qt.HighDpiScaleFactorRoundingPolicy.PassThrough
+            )
+            app = QApplication(sys.argv)
+            app.setAttribute(Qt.ApplicationAttribute.AA_DontCreateNativeWidgetSiblings)
+
+            _ = MainWindow()
+            sys.exit(app.exec())
+        except Exception:
+            import time
+            import traceback
+            from datetime import datetime
+
+            line = "─" * 20
+            header = f"{line} Crash reported at {time.asctime()} {line}\n"
+            content = traceback.format_exc()
+            footer = "─" * len(header)
+            print(f"{header}\n{content}\n{footer}")
+
+            crash_dir = Path(Path.cwd(), "crashes")
+            os.makedirs(crash_dir, exist_ok=True)
+            with open(
+                Path(
+                    crash_dir,
+                    f"crash {datetime.now().strftime('%Y-%m-%d')}.txt",
+                ),
+                "a",
+                encoding="utf-8",
+            ) as file:
+                file.write(f"{header}\n{content}\n\n\n")
---
+++ applib/module/configuration/internal/core_args.py
@@ -0,0 +1,50 @@
+import os
+from datetime import datetime
+from pathlib import Path
+
+from ....version import VERSION
+
+
+class CoreArgs:
+    # ┌───────────────────┐
+    # │ AppLib attributes │
+    # └───────────────────┘
+    # General
+    _core_app_name = "AppLib"
+    _core_app_version = VERSION
+    _core_link_github = "https://github.com/TheRealMorgenfrue/AppLib"
+    _core_is_release = False
+    _core_traceback_limit = 0 if _core_is_release else None
+    _core_app_dir = os.environ["APPLIB_PATH"]
+
+    # Logging
+    _core_log_name = _core_app_name
+    _core_log_dir = Path(_core_app_dir, "logs")
+    _core_log_format = "%(asctime)s - %(module)s - %(lineno)s - %(levelname)s - %(message)s"  # %(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    _core_log_use_color = True
+    _core_log_filename = datetime.now().strftime("%Y-%m-%d")
+    _core_log_disable_header = False
+
+    # Templates
+    _core_main_template_name = _core_app_name
+    _core_config_units = {}
+    _core_template_themes = ["Light", "Dark", "System"]
+
+    # Configs
+    _core_config_dir = Path(_core_app_dir, "configs")
+    _core_main_config_name = _core_app_name
+    _core_main_config_file = (
+        f"{_core_main_config_name.replace(' ', '_').lower()}_config.toml"
+    )
+    _core_main_config_path = str(Path(_core_config_dir, _core_main_config_file))
+
+    # Asset directories
+    _core_assets_dir = Path(_core_app_dir, "assets")
+    _core_logo_dir = Path(_core_assets_dir, "logos")
+    _core_icon_dir = Path(_core_assets_dir, "icons")  # Not used atm.
+    _core_images_dir = Path(_core_assets_dir, "images")
+    _core_qss_dir = Path(_core_assets_dir, "qss")
+
+    ## Asset paths
+    _core_main_logo_path = str(Path(_core_logo_dir, "logo.png"))
+    _core_banner_path = str(Path(_core_images_dir, "banner.jpg"))
