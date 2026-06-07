6b4fbea1ec9a6acb841208fe950c6b91915633f6
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2026-06-08 01:39:02 +0200

    Automatically create main logger when initializing LoggingManager

--- applib/module/logging/logger_utils.py
+++ applib/module/logging/logger_utils.py
@@ -1,16 +1,9 @@
+# Private AppLib module.
+
 import logging
 
 from ..configuration.internal.core_args import CoreArgs
-from . import LoggingManager
-
-
-def create_main_logger() -> logging.Logger:
-    return LoggingManager().create_logger(
-        name=CoreArgs._core_log_name,
-        format=CoreArgs._core_log_format,
-        log_dir=CoreArgs._core_log_dir,
-        log_filename=CoreArgs._core_log_filename,
-    )
+from .logging_manager import LoggingManager
 
 
 def create_basic_logger(name: str) -> logging.Logger:
@@ -23,8 +16,8 @@ def create_basic_logger(name: str) -> logging.Logger:
     )
 
 
-def write_header_to_log() -> None:
-    """Write startup header"""
+def maybe_write_header_to_log() -> None:
+    """Write startup header, if enabled"""
     if not CoreArgs._core_log_disable_header:
         logger_nocolor = create_basic_logger(f"{CoreArgs._core_log_name}_HEADER")
         padding = 90
